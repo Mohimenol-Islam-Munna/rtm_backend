e00daa126cb8ea2f09890318c859136afbdac44f
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-09-05 23:19:55 +0600

    connected by frontend

--- server.js
+++ server.js
@@ -10,20 +10,18 @@ const io = new Server(httpServer, {
   path: "/custom-socket-io",
 });
 
-console.log("IO :engine:", io.engine.clientscount);
-
 io.on("connection", (socket) => {
-  console.log("socket :::::", socket);
+  console.log("socket ::::: connected ::");
+
+  socket.on("message", (data) => {
+    console.log("Received message: ", data);
+  });
 });
-
-const adminSocket = io.of("/admin");
-
-console.log("adminSocket :::", adminSocket);
 
 app.get("/", (req, res) => {
   return res.json("This is home route");
 });
 
 httpServer.listen(8080, (req, res) => {
-  console.log("Server is running on port 8080");
+  console.log("Server is running... on port 8080");
 });
