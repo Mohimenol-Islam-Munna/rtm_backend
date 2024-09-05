554bd00711a3afb50d2a617a3d999e9b060a6da4
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-09-05 20:02:00 +0600

    socket server

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  path: "/custom-socket-io",
+});
+
+console.log("IO :engine:", io.engine.clientscount);
+
+io.on("connection", (socket) => {
+  console.log("socket :::::", socket);
+});
+
+const adminSocket = io.of("/admin");
+
+console.log("adminSocket :::", adminSocket);
 
 app.get("/", (req, res) => {
   return res.json("This is home route");
 });
 
-app.listen(8080, (req, res) => {
+httpServer.listen(8080, (req, res) => {
   console.log("Server is running on port 8080");
 });
